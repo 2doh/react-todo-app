d61f1e897e5d659cf64c46600ff43a46694a1f20
2doh <dev.ldh0973@naver.com>
2024-04-04 13:07:44 +0900

    할 일 앱 만들기

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,22 +1,23 @@
-import { MdAdd } from "react-icons/md";
 import { useState } from "react";
+import { MdAdd } from "react-icons/md";
 import "../styles/TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState("");
 
   const onChange = event => {
-    useState;
     setValue(event.target.value);
-    // console.log("입력하는 중",value)
   };
 
   const onSubmit = event => {
-    // Submit 이벤트는 브라우저에서 새로고침 발생시킴
+    onInsert(value);
+
+    // submit 이벤트는 브라우저에서 새로고침 발생시킴
     // 이를 방지하기 위해 아래 함수 호출
     event.preventDefault();
     setValue("");
   };
+
   return (
     <form className="TodoInsert" onSubmit={onSubmit}>
       <input
@@ -25,7 +26,7 @@ const TodoInsert = () => {
         value={value}
       />
       <button type="submit">
-        {/* 리엑트 아이콘 */}
+        {/* 리액트 아이콘 */}
         <MdAdd />
       </button>
     </form>
